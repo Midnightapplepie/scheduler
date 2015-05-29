d0115d403c3de9dc61babe3e7104aef63c80d9b3
midnightapplepie <midnightapplepie@gmail.com>
2015-05-28 23:32:53 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,42 @@
-require 'faker'
+# def generate_employee
+#   days = [1,2,3,4,5,6,7]
+#   roles = ["host", "buser", "delivery", "kitchen"]
+#   n = rand(1..5)
+#   na = days.sample(n+2)
+#   tasks = roles.sample(2)
+  
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+  
+#   minimum_days_required = n
+#   tasks = tasks
+#   full_time = (n == 5)? true : false
+#   phone_number = Faker::PhoneNumber.cell_phone
+  
+#   employee = Employee.create(
+#     first_name: first_name, 
+#     last_name: last_name, 
+#     minimum_days_required: minimum_days_required, 
+#     phone_number: phone_number, 
+#     full_time: full_time,
+#     day_availabilities_text: na.take(rand(0..na.count)).join(","),
+#     night_availabilities_text:  na.join(","),
+#     preferred_days_text: na.sample(n).join(","),
+#     roles_text: tasks.join(",")
+#     )
+# end
 
-def generate_employee
-  days = [1,2,3,4,5,6,7]
-  roles = ["host", "buser", "delivery", "kitchen"]
-  n = rand(1..5)
-  na = days.sample(n+2)
-  tasks = roles.sample(2)
-  
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  
-  minimum_days_required = n
-  tasks = tasks
-  full_time = (n == 5)? true : false
-  phone_number = Faker::PhoneNumber.cell_phone
-  
-  employee = Employee.create(
-    first_name: first_name, 
-    last_name: last_name, 
-    minimum_days_required: minimum_days_required, 
-    phone_number: phone_number, 
-    full_time: full_time,
-    day_availabilities_text: na.take(rand(0..na.count)).join(","),
-    night_availabilities_text:  na.join(","),
-    preferred_days_text: na.sample(n).join(","),
-    roles_text: tasks.join(",")
-    )
-end
-
-# Employee.create(
-#   first_name: "Matt",
-#   last_name: "Matt",
-#   minimum_days_required: 5,
-#   phone_number: Faker::PhoneNumber.cell_phone,
-#   full_time: true,
-#   morning_avail_text: [1,2,3,4,5,6,7].join(","),
-#   night_avail_text: [1,2,3,4,5,6,7].join(","),
-#   preferred_days_text: [2,3,4,5,6].join(","),
-#   roles_text: ["host","buser"].join(",")
-#   )
+Employee.create(
+  first_name: "Matt",
+  last_name: "Matt",
+  minimum_days_required: 5,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  full_time: true,
+  morning_avail_text: [1,2,3,4,5,6,7].join(","),
+  night_avail_text: [1,2,3,4,5,6,7].join(","),
+  preferred_days_text: [2,3,4,5,6].join(","),
+  roles_text: ["host","buser"].join(",")
+  )
 
 Employee.create(
   first_name: "Justin",
@@ -112,17 +110,17 @@ Employee.create(
   roles_text: ["host","buser"].join(",")
   )
 
-# Employee.create(
-#   first_name: "Taylor", 
-#   last_name: "Taylor",
-#   minimum_days_required: 3,
-#   phone_number: Faker::PhoneNumber.cell_phone,
-#   full_time: false,
-#   morning_avail_text: [2].join(","),
-#   night_avail_text: [1,2,7].join(","),
-#   preferred_days_text: [1,2,7].join(","),
-#   roles_text: ["host","host"].join(",")
-#   )
+Employee.create(
+  first_name: "Taylor", 
+  last_name: "Taylor",
+  minimum_days_required: 3,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  full_time: false,
+  morning_avail_text: [2].join(","),
+  night_avail_text: [1,2,7].join(","),
+  preferred_days_text: [1,2,7].join(","),
+  roles_text: ["host","host"].join(",")
+  )
 
 Employee.create(
   first_name: "Jerry",
